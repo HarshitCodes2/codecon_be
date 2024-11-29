884b83603cde582c841e173f2301c49260acc0e8
HarshitCodes2 <g1harshit7@gmail.com>
2024-11-29 11:52:52 +0530

    Unresponsive CodeCon Front End

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import controller from "./routes/controller";
-require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -9,8 +8,6 @@ app.use(cors());
 
 app.use("/api", controller);
 
-app.listen(process.env.PORT, () =>
-  console.log("Listening at port : " + process.env.PORT)
-);
+app.listen(3000, () => console.log("Listening at port : " + 3000));
 
 export default app;
